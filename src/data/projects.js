49157541b95dda03d10e938599de4f52a8ace59b
Andreas Prang <mail@andreasprang.de>
2017-11-28 22:58:24 +0100

    fixed ownerId

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,20 +3,23 @@ export default [
         "title": "polymorphia",
         "description": "A very fast POJO codec for MongoDB (used in conjunction with the Mongo Java Driver) that handles generic types as well as polymorphic class hierarchies",
         "videoUrl": "",
-        "ownerId": "bild",
+        "ownerId": "axelspringer",
+        "ownerLogo": "bild",
         "projectId": "polymorphia"
     },{
         "title": "crowdsource",
         "description": "a collaborative crowd sourcing and funding tool using virtual currency",
         "videoUrl": "https://www.youtube.com/watch?v=7hbZAEqHsOY",
-        "ownerId": "asideas",
+        "ownerId": "axelspringer",
+        "ownerLogo": "bild",
         "projectId": "crowdsource"
     },
     {
         "title": "ideas-dash",
         "description": "dashboard / team monitor for software development teams",
         "videoUrl": "https://www.youtube.com/watch?v=7hbZAEqHsOY",
-        "ownerId": "asideas",
+        "ownerId": "axelspringer",
+        "ownerLogo": "bild",
         "projectId": "ideas-dash"
     },
     {
@@ -24,18 +27,21 @@ export default [
         "description": "Python package to evaluate the quality of semantic engines",
         "videoUrl": "https://www.youtube.com/watch?v=7hbZAEqHsOY",
         "ownerId": "axelspringer",
+        "ownerLogo": "bild",
         "projectId": "semaeval"
     }, {
         "title": "tech-radar",
         "description": "The Tech Radar is a tool to inspire and support teams to pick the best technologies for new projects",
         "videoUrl": "https://www.youtube.com/watch?v=7hbZAEqHsOY",
-        "ownerId": "bild",
+        "ownerId": "axelspringer",
+        "ownerLogo": "bild",
         "projectId": "tech-radar"
     }, {
         "title": "axelspringer.github.io",
         "description": "This project is the landing page for the open source initiative of Axel Springer SE",
         "videoUrl": "https://www.youtube.com/watch?v=7hbZAEqHsOY",
         "ownerId": "axelspringer",
+        "ownerLogo": "bild",
         "projectId": "axelspringer.github.io"
     }
 ]
