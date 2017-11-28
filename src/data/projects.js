40d21e43a5edaf07c5b7d99b357f5b98514d3831
Andreas Prang <mail@andreasprang.de>
2017-11-28 19:34:26 +0100

    changed orderId for polymorphia

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export default [
         "title": "polymorphia",
         "description": "A very fast POJO codec for MongoDB (used in conjunction with the Mongo Java Driver) that handles generic types as well as polymorphic class hierarchies",
         "videoUrl": "",
-        "ownerId": "axelspringer",
+        "ownerId": "bild",
         "projectId": "polymorphia"
     },{
         "title": "crowdsource",
