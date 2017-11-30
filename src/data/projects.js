17c28953d15e25a295f58c31c4e752d9eb6d1962
Hanna <hanna.behnke@gmail.com>
2017-11-30 16:55:47 +0100

    Adds WeltN24 projects to Landing Page

--- src/data/projects.js
+++ src/data/projects.js
@@ -49,5 +49,113 @@ export default [
         "ownerLogo": "axelspringer",
         "projectId": "axelspringer.github.io",
         "language": "node.js"
+    },
+    {
+        "title": "Carlos",
+        "description": "A simple but flexible cache",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "Carlos",
+        "language": "Swift"
+    },
+    {
+        "title": "ecr-cleaner",
+        "description": "Deletes old images from ecr",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "ecr-cleaner",
+        "language": "GO"
+    },
+    {
+        "title": "react-web-component",
+        "description": "Create Web Components with React",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "react-web-component",
+        "language": "React"
+    },
+    {
+        "title": "PiedPiper",
+        "description": "A small set of classes and functions to make easy use of Futures, Promises and async computation in general",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "PiedPiper",
+        "language": "Swift"
+    },
+    {
+        "title": "LazyPages",
+        "description": "A highly customizable library that helps you to show a scrollable list of view controllers synchronized with an index",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "LazyPages",
+        "language": "Swift"
+    },
+    {
+        "title": "LazyPages",
+        "description": "LazyPages can be used in a monitoring systems like nagios or zabbix to discover items on aws",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "LazyPages",
+        "language": "GO"
+    },
+    {
+        "title": "metrics-beaver",
+        "description": "fetches metrics from cloudwatch",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "metrics-beaver",
+        "language": "GO"
+    },
+    {
+        "title": "aws-mfa-detector",
+        "description": "Fetches all users from an aws account and returns the once which don't have activated MFA",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "aws-mfa-detector",
+        "language": "GO"
+    },
+    {
+        "title": "CleanAndroidCookbook",
+        "description": "Set of Clean Android recipes using Kotlin, Dagger, RxJava, Databinding",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "CleanAndroidCookbook",
+        "language": "Java"
+    },
+    {
+        "title": "gradle-spring-boot-conventions-plugin",
+        "description": "Best practices and conventions for developing Spring Boot microservices",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "gradle-spring-boot-conventions-plugin",
+        "language": "Groovy"
+    },
+    {
+        "title": "munch-bot",
+        "description": "Is it worth going to the canteen today?",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "munch-bot",
+        "language": "JavaScript"
+    },
+    {
+        "title": "GraphQLicious",
+        "description": "A swift component with a DSL to declare GraphQL queries and to get string representations out of them",
+        "videoUrl": "https://youtu.be/YAbpmkqn6JE?t=45s",
+        "ownerId": "WeltN24",
+        "ownerLogo": "WeltN24",
+        "projectId": "GraphQLicious",
+        "language": "Swift"
     }
 ]
